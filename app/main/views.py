10bc465b789a3d3223a9f42c6103588c89cb53dc
xdhuxc <xdhuxc@163.com>
2018-09-12 23:09:22 +0800

    完成布局，正式开始页面。

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,21 @@
 from flask import render_template
 from ..main import main
 
+from kubernetes import client
+from kubernetes import config
+
 
 @main.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@main.route('/pod')
+def manage_pod():
+    client.Configuration.host = '172.20.26.150'
+
+    v1 = client.CoreV1Api()
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for item in ret.items():
+        print(item)
+    return render_template('pod.html')

ad30e619501ae7f2850915e5bac026fbae670ac5
xdhuxc <xdhuxc@163.com>
2018-09-25 21:04:23 +0800

    创建 Deployment。

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from flask_wtf import FlaskForm
+
+from wtforms import StringField
+from wtforms import IntegerField
+from wtforms import SubmitField
+
+from wtforms.validators import DataRequired
+from wtforms.validators import Length
+
+
+class Deployment(FlaskForm):
+    image_name = StringField('镜像名称', validators=[DataRequired()])
+    replicas = IntegerField('副本数量', validators=[DataRequired()])
+    submit = SubmitField('创建')
+

5f56e6c14f3c4ea2758972f472a4680bc2b31424
xdhuxc <xdhuxc@163.com>
2018-09-28 15:30:23 +0800

    添加日志组件。

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 from config import config
 from kubernetes import config as kubernetes_config
 from kubernetes import client as kubernetes_client
+import logging
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -18,8 +19,6 @@ kubernetes_config.load_kube_config()
 # 创建 kubernetes 客户端对象
 kclient = kubernetes_client.CoreV1Api()
 oclient = kubernetes_client
-
-# 日志记录器
 
 
 def create_app(config_name):
@@ -36,4 +35,36 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     return app
+
+
+def create_logger():
+    """
+    创建一个日志记录器并返回
+    :return:
+    """
+    # 创建一个 logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # 创建一个 FileHandler 对象，用于写入日志文件
+    file_handler = logging.FileHandler('xdevops.log')
+    file_handler.setLevel(logging.DEBUG)
+
+    # 创建一个 StreamHandler 对象，用于输出到控制台
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+
+    # 定义 handler 的输出格式
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+
+    # 给 logger 添加处理器
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+# 日志记录器
+logger = create_logger()
 
--- app/utils.py
+++ app/utils.py
@@ -6,7 +6,6 @@
 
 import os
 import yaml
-import logging
 
 from . import kclient
 
@@ -199,33 +198,6 @@ class Utils(object):
         spec = V1beta1IngressSpec(rules=rules)
         return V1beta1Ingress(api_version=api_version, kind=kind, metadata=metadata, spec=spec)
 
-    @staticmethod
-    def get_logger():
-        """
-        创建一个日志记录器并返回
-        :return:
-        """
-        # 创建一个 logger
-        logger = logging.getLogger()
-        logger.setLevel(logging.DEBUG)
 
-        # 创建一个 FileHandler 对象，用于写入日志文件
-        file_handler = logging.FileHandler('xdevops.log')
-        file_handler.setLevel(logging.DEBUG)
 
-        # 创建一个 StreamHandler 对象，用于输出到控制台
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.DEBUG)
 
-        # 定义 handler 的输出格式
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
-
-        # 给 logger 添加处理器
-        logger.addHandler(file_handler)
-        logger.addHandler(stream_handler)
-
-        return logger
-
-    logger = Utils.get_logger()

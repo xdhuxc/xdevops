1504a9355283e469569349c871b8b602697dc3b9
xdhuxc <xdhuxc@163.com>
2018-10-09 16:51:26 +0800

    Python 单元测试。

---
+++ test/mathfunc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: wanghuan
+# date: 2018-09-09
+# description:
+
+
+def add(x, y):
+    return x + y
+
+
+def minus(x, y):
+    return x - y
+
+
+def multi(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
--- test/test_math.py
+++ test/test_math.py
@@ -7,11 +7,37 @@
 import unittest
 from mathfunc import *
 
+
 class TestMathFunc(unittest.TestCase):
 
+    def setUp(self):
+        pass
+
     def test_add(self):
-        self.assertTrue(3, add(1,2))
+        self.assertEqual(3, add(1, 2))
+        self.assertNotEqual(2, add(1, 2))
+
+    def test_minus(self):
+        self.assertEqual(1, minus(3, 2))
+
+    def test_multi(self):
+        self.assertEqual(6, multi(2, 3))
+
+    def test_divide(self):
+        self.assertEqual(2, divide(6, 3))
+        #self.assertEqual(2.5, divide(5, 2))
+
+    def tearDown(self):
+        pass
+
 
 if __name__ == '__main__':
-    unittest.main()
+
+    test_runner = unittest.TextTestRunner()
+    test_suite = unittest.TestSuite()
+    test_suite.addTests(map(TestMathFunc, ['test_add', 'test_minus', 'test_multi', 'test_divide']))
+    test_runner.run(test_suite)
+
+    # unittest.main(verbosity=2)
+
 
